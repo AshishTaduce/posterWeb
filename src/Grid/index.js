cb2b4d41a9fecf2f99d2bd2c353bde759a49c9e1
AshishTaduce <ashishteddy2@gmail.com>
2020-05-09 18:15:36 +0530

    Search page reworked to include a button for search, instead of instantaneous response

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MoviesList from "../MoviesList"
-import {Link, BrowserRouter as Router} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Grid extends React.Component {
 
--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -7,6 +7,7 @@ export default class SearchPage extends React.Component{
         super(props);
         this.state = {
             searchQuery: '',
+            query: '',
         }
     }
 
@@ -18,20 +19,30 @@ export default class SearchPage extends React.Component{
         })
     }
 
+    searchMovies(){
+        this.setState({
+            query: this.state.searchQuery,
+        })
+    }
+
     render() {
         const api = `&api_key=a68598b6e3e81567486644082b967d8f`;
         let endpoint = `https://api.themoviedb.org/3/search/movie?query=${this.state.searchQuery}${api}`;
 
         return (
             <div className={'main-body'}>
-                    <input
+                    <div >
+                        <input className={'search-bar'}
                         autoFocus="autofocus"
-                        className={'search-bar'}
                         type={'text'} title={'Search'}
                         aria-label={'Search'}
                         onChange={(e) => this.changeQuery(e)}/>
+                        <button className={'search-button'}
+                        onClick={() => this.searchMovies()}>
+                            Search</button>
+                    </div>
 
-                <MoviesList key = {this.state.searchQuery} fromSearchPage = {true} movieUrl = {this.state.searchQuery.trim() === '' ? undefined : endpoint}/>
+                <MoviesList key = {this.state.query} fromSearchPage = {true} movieUrl = {this.state.query.trim() === '' ? undefined : endpoint}/>
             </div>
         );
     }
