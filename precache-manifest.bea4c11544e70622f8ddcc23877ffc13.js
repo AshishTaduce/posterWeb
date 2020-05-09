f22df4b79ad5f92eb3c53235908cbfd8c622f130
AshishTaduce <ashishteddy2@gmail.com>
2020-05-09 18:18:27 +0530

    Updates

---
+++ precache-manifest.bea4c11544e70622f8ddcc23877ffc13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285e40b9d15ad987dfe6a0f041db1021",
+    "url": "/posterWeb/index.html"
+  },
+  {
+    "revision": "42c931efc60f4dfaf387",
+    "url": "/posterWeb/static/css/main.07cefcf0.chunk.css"
+  },
+  {
+    "revision": "4275eb5b2f9e26f263b8",
+    "url": "/posterWeb/static/js/2.fb4b1c50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/posterWeb/static/js/2.fb4b1c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c931efc60f4dfaf387",
+    "url": "/posterWeb/static/js/main.f0769a1d.chunk.js"
+  },
+  {
+    "revision": "53be4109535e3aff0eff",
+    "url": "/posterWeb/static/js/runtime-main.c45abdf0.js"
+  }
+]);
